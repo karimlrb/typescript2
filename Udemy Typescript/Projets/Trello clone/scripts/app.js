664cd978970852528ed56e13819b83214f397d0b
karimlrb <japan-rimk@hotmail.fr>
2021-11-03 16:22:18 +0100

    Add an other container fonctionnel, manque le d&drop

--- Udemy Typescript/Projets/Trello clone/scripts/app.js
+++ Udemy Typescript/Projets/Trello clone/scripts/app.js
@@ -101,3 +101,61 @@ function handleItemDeletion(btn) {
         liToRemove.remove();
     });
 }
+//  Add New Container
+const addContainerBtn = document.querySelector('.add-container-btn');
+const addContainerForm = document.querySelector('.add-new-container form');
+const addContainerFormInput = document.querySelector('.add-new-container input');
+const validationNewContainer = document.querySelector('.add-new-container .validation-msg');
+// Attention a select addContainerCloseBtn avec .close-add-list et pas close-form-btn sinon ça marche pas
+const addContainerCloseBtn = document.querySelector('.close-add-list');
+const addNewContainer = document.querySelector('.add-new-container');
+const containersList = document.querySelector('.main-content');
+// Ouverture du form au click sur Add an other btn
+addContainerBtn.addEventListener("click", () => {
+    toggleForm(addContainerBtn, addContainerForm, true);
+});
+// Fermeture du form au click sur la x
+addContainerCloseBtn.addEventListener("click", () => {
+    toggleForm(addContainerBtn, addContainerForm, false);
+});
+// Au submit il faut ecrire le V-msg ou ecrire l'input
+addContainerForm.addEventListener("submit", createNewContainer);
+function createNewContainer(e) {
+    e.preventDefault();
+    // Ici ce n'est pas actualInput mais addContainerFormInput car on est dans 4ème block apart
+    if (addContainerFormInput.value.length === 0) {
+        validationNewContainer.textContent = "Must be at least 1 character long";
+        return;
+    }
+    else {
+        validationNewContainer.textContent = "";
+    }
+    const itemsContainer = document.querySelector('.items-container');
+    //  cloneNode -> méthode qui permet de cloner un noeu
+    const newContainer = itemsContainer.cloneNode();
+    const newContainerContent = `
+     <div class="top-container">
+       <h2>${addContainerFormInput.value}</h2>
+       <button class="delete-container-btn">x</button>
+     </div>
+     <ul></ul>
+     <button class="add-item-btn">Add an item</button>
+     <form autocomplete="off">
+       <div class="top-form-container">
+         <label for="item">Add a new item</label>
+         <button type="button" class="close-form-btn">x</button>
+       </div>
+       <input type="text" id="item" />
+       <span class="validation-msg"></span>
+       <button type="submit">Submit</button>
+     </form>
+     `;
+    // Node.insertBefore() insère un nœud avant un nœud de référence en tant qu'enfant d'un nœud parent spécifié.
+    // On vient l'insérer juste avant le addNewContainer
+    newContainer.innerHTML = newContainerContent;
+    containersList.insertBefore(newContainer, addNewContainer);
+    //  On vide le'inputet on rapelle addContainerListeners
+    // On crée un container mais il faut lui ajouter tous les eventlisteners
+    addContainerFormInput.value = "";
+    addContainerListeners(newContainer);
+}
