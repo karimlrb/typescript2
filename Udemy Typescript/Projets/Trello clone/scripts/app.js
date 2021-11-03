86c512fc8718c1799576d6b18c2aabea7e63f5b9
karimlrb <japan-rimk@hotmail.fr>
2021-11-03 05:05:22 +0100

    Ajout du toggle sur le bouton addItem

--- Udemy Typescript/Projets/Trello clone/scripts/app.js
+++ Udemy Typescript/Projets/Trello clone/scripts/app.js
@@ -1,14 +1,20 @@
 "use strict";
 const itemsContainer = document.querySelectorAll('.items-container');
+let actualContainer, actualBtn, actualUL, actualForm, actualTextInput, actualValidation;
 // Function qui sert à rajouter tous les listeners
 function addContainerListeners(currentContainer) {
     // On trouve le bon bouton ici
     const currentContainerDeletionBtn = currentContainer.querySelector('.delete-container-btn');
+    const currentAddItemBtn = currentContainer.querySelector('.add-item-btn');
     // On lui rajoute l'eventListeners
     deleteBtnListeners(currentContainerDeletionBtn);
+    addItemBtnListeners(currentAddItemBtn);
 }
 function deleteBtnListeners(btn) {
     btn.addEventListener("click", handleContainerDeletion);
+}
+function addItemBtnListeners(btn) {
+    btn.addEventListener("click", handleAddItem);
 }
 itemsContainer.forEach((container) => {
     addContainerListeners(container);
@@ -20,4 +26,32 @@ function handleContainerDeletion(e) {
     const containers = [...document.querySelectorAll('.items-container')];
     // On cherche l'index de btn (le bouton cliqué), une fois trouvé on supprime le container car il a le même
     containers[btnsArray.indexOf(btn)].remove();
+}
+function handleAddItem(e) {
+    const btn = e.target;
+    // Si actualContainer j'ai cliqué et il y'en a un ouvert, donc si c'est vrai j'apple toggleForm et je le ferme avec false
+    if (actualContainer) {
+        toggleForm(actualBtn, actualForm, false);
+    }
+    setContainerItems(btn);
+    toggleForm(actualBtn, actualForm, true);
+}
+function toggleForm(btn, form, action) {
+    if (!action) {
+        form.style.display = "none";
+        btn.style.display = "block";
+    }
+    else if (action) {
+        form.style.display = "block";
+        btn.style.display = "none";
+    }
+}
+// Dès qu'in clique sur un container on a accés à tous ses éléments inside
+function setContainerItems(btn) {
+    actualBtn = btn;
+    actualContainer = btn.parentElement;
+    actualUL = actualContainer.querySelector('ul');
+    actualForm = actualContainer.querySelector('form');
+    actualTextInput = actualContainer.querySelector('input');
+    actualValidation = actualContainer.querySelector('.validation-msg');
 }
--- Udemy Typescript/Projets/Trello clone/src/app.ts
+++ Udemy Typescript/Projets/Trello clone/src/app.ts
@@ -1,15 +1,28 @@
 const itemsContainer = document.querySelectorAll('.items-container') as NodeListOf<HTMLDivElement>;
 
+let actualContainer: HTMLDivElement,
+    actualBtn:HTMLButtonElement,
+    actualUL:HTMLUListElement,
+    actualForm:HTMLFormElement,
+    actualTextInput:HTMLInputElement,
+    actualValidation:HTMLSpanElement;
+
 // Function qui sert à rajouter tous les listeners
 function addContainerListeners(currentContainer:HTMLDivElement){
     // On trouve le bon bouton ici
     const currentContainerDeletionBtn = currentContainer.querySelector('.delete-container-btn') as HTMLButtonElement;
+    const currentAddItemBtn = currentContainer.querySelector('.add-item-btn') as HTMLButtonElement;
     // On lui rajoute l'eventListeners
-    deleteBtnListeners(currentContainerDeletionBtn)
+    deleteBtnListeners(currentContainerDeletionBtn);
+    addItemBtnListeners(currentAddItemBtn);
 }
 
 function deleteBtnListeners(btn: HTMLButtonElement){
-    btn.addEventListener("click", handleContainerDeletion)
+    btn.addEventListener("click", handleContainerDeletion);
+}
+
+function addItemBtnListeners(btn:HTMLButtonElement){
+    btn.addEventListener("click", handleAddItem);
 }
 
 itemsContainer.forEach((container:HTMLDivElement) => {
@@ -25,3 +38,32 @@ function handleContainerDeletion(e :MouseEvent){
     containers[btnsArray.indexOf(btn)].remove();
 }
 
+function handleAddItem(e:MouseEvent){
+    const btn = e.target as HTMLButtonElement;
+    // Si actualContainer j'ai cliqué et il y'en a un ouvert, donc si c'est vrai j'apple toggleForm et je le ferme avec false
+    if (actualContainer) {
+        toggleForm(actualBtn, actualForm, false)
+    }
+    setContainerItems(btn);
+    toggleForm(actualBtn, actualForm, true);
+}
+
+function toggleForm(btn:HTMLButtonElement, form:HTMLFormElement,action:boolean){
+    if (!action) {
+        form.style.display="none";
+        btn.style.display="block";
+    }else if (action){
+        form.style.display="block";
+        btn.style.display="none";
+    }
+}
+
+// Dès qu'in clique sur un container on a accés à tous ses éléments inside
+function setContainerItems(btn: HTMLButtonElement){
+    actualBtn = btn;
+    actualContainer = btn.parentElement as HTMLDivElement;
+    actualUL = actualContainer.querySelector('ul') as HTMLUListElement;
+    actualForm = actualContainer.querySelector('form') as HTMLFormElement;
+    actualTextInput = actualContainer.querySelector('input') as HTMLInputElement;
+    actualValidation = actualContainer.querySelector('.validation-msg') as HTMLSpanElement;
+}
